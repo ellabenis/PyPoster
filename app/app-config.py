fc6991a0fd48267594de265c288b2dacb1cb6828
michaelbenis <Michael@Benis.co.il>
2023-04-12 16:28:11 +0300

    db name

--- app/app-config.py
+++ app/app-config.py
@@ -4,9 +4,14 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
 MONGODB_HOST = os.environ.get('MONGODB_HOST') or 'mongodb'
 MONGODB_PORT = int(os.environ.get('MONGODB_PORT') or 27017)
-MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME') or 'admin'
-MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD') or 'admin'
-MONGODB_DB = os.environ.get('MONGODB_DB') or 'pyposterdb'
+
+MONGODB_USERNAME = 'admin'
+MONGODB_PASSWORD = 'admin'
+MONGODB_DB = 'pyposterdb'
+
+# MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME') or 'admin'
+# MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD') or 'admin'
+# MONGODB_DB = os.environ.get('MONGODB_DB') or 'pyposterdb'
 
 # API key for movie poster images
 API_KEY = 'MY TMDB API KEY'
